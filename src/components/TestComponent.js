091cbbeead17757e7fb3ca0fcbdcfbaf995210dc
Jason Artley <jtartley@yahoo.com>
2024-04-29 18:17:15 -0700

    cleared base file to create a blank canvas, added test component

---
+++ src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TestComponent() {
+  return (
+    <div>
+      <h1>Hello World! - from TestComponent()</h1>
+    </div>
+  );
+}
+
+export default TestComponent;
