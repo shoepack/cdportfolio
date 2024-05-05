f56060199be0bb9acfed4b1c850ad7fd7dabab9c
shoepack <yarmoam@gmail.com>
2024-05-05 15:23:39 -0700

    added note

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,6 +8,7 @@ function HeroSection() {
         Hey, I'm Jason - <span className="highlight"> Front-End Developer</span>
       </h1>
       <p>I'm a designer & developer based in La Jolla, CA!</p>
+      <p>Note: Page is currently being worked on - more changes soon!</p>
     </section>
   );
 }
