00519a653833aeb801d69900ec6e82e48175e5fd
Jason Artley <jtartley@yahoo.com>
2025-06-28 10:21:42 -0700

    Update hero animation handling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,9 +11,15 @@ const container = (delay) => ({
   },
 });
 
+const imageAnimation = {
+  initial: { x: 100, opacity: 0 },
+  animate: { x: 0, opacity: 1 },
+  transition: { duration: 1, delay: 1.2 },
+};
+
 const Hero = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+    <section className="border-b border-neutral-900 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
@@ -26,7 +32,7 @@ const Hero = () => {
               Jason Artley
             </motion.h1>
             <motion.span
-              variants={container(0.3)}
+              variants={container(0.5)}
               initial="hidden"
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
@@ -34,7 +40,7 @@ const Hero = () => {
               Full Stack Developer
             </motion.span>
             <motion.p
-              variants={container(0.6)}
+              variants={container(1)}
               initial="hidden"
               animate="visible"
               className="my-2 max-w-xl py-6 font-light tracking-tighter"
@@ -46,16 +52,14 @@ const Hero = () => {
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
             <motion.img
-              initial={{ x: 100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 1, delay: 1.0 }}
+              {...imageAnimation}
               src={profilePic}
               alt="Jason Artley"
             />
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
