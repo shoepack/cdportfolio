9047b965b6188054fcbd586212f80bce78dbb0e3
Jason Artley <jtartley@yahoo.com>
2025-10-25 12:29:07 -0700

    Fix margin for technology tags in Experience and Projects components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -41,7 +41,7 @@ const Projects = () => {
                 {project.technologies.map((tech, index) => (
                   <span
                     key={index}
-                    className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-green-500"
+                    className="mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-green-500"
                   >
                     {tech}
                   </span>
